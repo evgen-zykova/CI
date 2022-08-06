eea479d348b33e48c367918ad7b7bb7775c6d56f
Евгения Пискарева <evgen-zykova@mail.ru>
2022-08-07 00:24:15 +0300

    json

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
@@ -18,13 +19,10 @@ class MobileBankApiTestV3 {
       .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
+              .then()
+              .statusCode(200)
+              // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
     }
 }
